831998756627f55b87d2be6d8f6eac3a89be0374
mostafijur-devs <mostafijur.devs@gmail.com>
2025-05-26 14:09:02 +0600

    create some screens

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,24 +9,24 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
-dependencies {
-    // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
-
-
-    // TODO: Add the dependencies for Firebase products you want to use
-    // When using the BoM, don't specify versions in Firebase dependencies
-    implementation("com.google.firebase:firebase-analytics")
-
-
-    // Add the dependencies for any other desired Firebase products
-    // https://firebase.google.com/docs/android/setup#available-libraries
-}
+//dependencies {
+//    // Import the Firebase BoM
+//    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+//
+//
+//    // TODO: Add the dependencies for Firebase products you want to use
+//    // When using the BoM, don't specify versions in Firebase dependencies
+//    implementation("com.google.firebase:firebase-analytics")
+//
+//
+//    // Add the dependencies for any other desired Firebase products
+//    // https://firebase.google.com/docs/android/setup#available-libraries
+//}
 
 android {
     namespace = "com.example.blog_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+//    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -42,7 +42,7 @@ android {
         applicationId = "com.example.blog_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
